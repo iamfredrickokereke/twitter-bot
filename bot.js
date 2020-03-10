db327898e9cdd78075ec34f1901d1aca6e1324f0
Kelly online <kellyonline2016@gmail.com>
2020-03-11 00:01:38 +0100

    add secret auth keys

--- bot.js
+++ bot.js
@@ -2,3 +2,9 @@
 
 var Twit = require('twit');
 
+var T = new Twit({
+    consumer_key:         'YOURCONSUMERKEY',
+    consumer_secret:      'YOURCONSUMERSECRET',
+    access_token:         'YOURACCESSTOKEN',
+    access_token_secret:  'YOURACCESSTOKENSECRET'
+  });
