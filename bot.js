905984b387603f947217990ace1e6448af24c026
Kelly online <kellyonline2016@gmail.com>
2020-03-11 23:54:23 +0100

    added comments

--- bot.js
+++ bot.js
@@ -1,7 +1,12 @@
 // console.log("Testing the console script")
 
+
+// import twit from node modules
+
 var Twit = require('twit');
 
+
+// get auth connection keys
 var T = new Twit({
     consumer_key:         'YOURCONSUMERKEY',
     consumer_secret:      'YOURCONSUMERSECRET',
